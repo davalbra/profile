9ca301a61d1fe133185226d92b064090d3cd788e
davalbra <davalbra@espol.edu.ec>
2026-02-17 14:38:22 -0500

    feat: Agregué la gestión de carga de imágenes optimizadas utilizando Suspense.

--- app/dashboard/images/copies/page.tsx
+++ app/dashboard/images/copies/page.tsx
@@ -1,10 +1,13 @@
+import {Suspense} from "react";
 import {ImageCopiesManager} from "@/components/dashboard/image-copies-manager"
 
 export default function DashboardImagesCopiesPage() {
     return (
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
             <div className="px-4 lg:px-6">
-                <ImageCopiesManager/>
+                <Suspense fallback={null}>
+                    <ImageCopiesManager/>
+                </Suspense>
             </div>
         </div>
     )
--- app/dashboard/images/optimize/page.tsx
+++ app/dashboard/images/optimize/page.tsx
@@ -1,10 +1,13 @@
+import {Suspense} from "react";
 import {ImagesManager} from "@/components/dashboard/images-manager"
 
 export default function DashboardImagesOptimizePage() {
     return (
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
             <div className="px-4 lg:px-6">
-                <ImagesManager/>
+                <Suspense fallback={null}>
+                    <ImagesManager/>
+                </Suspense>
             </div>
         </div>
     )
