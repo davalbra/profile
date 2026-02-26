298e24fd9b12c2aa4ea0e2d1c9fd4d772f87001a
davalbra <davalbra@espol.edu.ec>
2026-02-26 14:47:35 -0500

    feat: Agregué orígenes permitidos para el desarrollo en la configuración de Next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: [
+    "https://davalbra.xyz",
+    "http://davalbra.xyz",
+    "https://www.davalbra.xyz",
+    "http://www.davalbra.xyz",
+  ],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "raw.githubusercontent.com" },
