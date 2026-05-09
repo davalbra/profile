1f72659c5690a0056dc858952ad30a96fef7cc11
davalbra <davalbra@espol.edu.ec>
2026-05-08 20:51:39 -0500

    refactor: mejorar la configuración de ESLint para componentes Vue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,17 @@
 import withNuxt from "./.nuxt/eslint.config.mjs"
 
-export default withNuxt({
-  ignores: [".output/**", ".nuxt/**", "out/**", "build/**", "dist/**"],
-  rules: {
-    "vue/multi-word-component-names": "off",
-    "vue/html-self-closing": "off",
+export default withNuxt(
+  {
+    ignores: [".output/**", ".nuxt/**", "out/**", "build/**", "dist/**"],
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "vue/html-self-closing": "off",
+    },
   },
-})
+  {
+    files: ["components/ui/**/*.vue"],
+    rules: {
+      "vue/require-default-prop": "off",
+    },
+  },
+)
